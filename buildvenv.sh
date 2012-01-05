355abaaf05d63cc114a4c2bc2d936eb9ac784234
jeff <jeffbuttars@gmail.com>
2012-01-04 17:53:07 -0700

    initial checkin of prebuilt django project with a grappelli admin

---
+++ buildvenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+venvname='venv'
+venvopts='--no-site-packages'
+
+djangoproj='grappellibase'
+
+# Install a virutalvenv
+virtualenv $venvopts $venvname
+
+. $venvname/bin/activate
+
+# If there are requirements, install them.
+if [[ -f requirements.txt ]]; then
+	pip install $(cat ./requirements.txt)
+fi
+
+python $djangoproj/manage.py syncdb
+python $djangoproj/manage.py runserver
