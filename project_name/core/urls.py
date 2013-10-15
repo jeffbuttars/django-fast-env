fc147c6f6d102d43d238fd197fe4d49aef18e125
Jeff Buttars <jeffbuttars@gmail.com>
2013-10-15 15:51:57 -0600

    Add a CoreView base class for some easier boiler plate views.

--- project_name/core/urls.py
+++ project_name/core/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns(
     url(r'^logout/$',
         'django.contrib.auth.views.logout',
         {'next_page': '/'}),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index.as_view(), name='index'),
 )
--- project_name/core/views.py
+++ project_name/core/views.py
@@ -1,15 +1,40 @@
+from django.views.generic.base import View
+from django.utils.decorators import method_decorator
 from annoying.decorators import render_to
 from core.forms import BSAuthenticationForm
 
 
-def default_data():
-    return {
-        'login_form': BSAuthenticationForm,
-            }
-# default_data()
+class CoreView(View):
+
+    """Docstring for CoreView """
+
+    def get(self, req):
+        """todo: Docstring for get
+
+        :param req: arg description
+        :type req: type description
+        :return:
+        :rtype:
+        """
+
+        return {
+            'login_form': BSAuthenticationForm,
+        }
+    # get()
+# CoreView
 
 
-@render_to("core_index.html")
-def index(req):
-    return default_data()
-# index()
+class index(CoreView):
+
+    @method_decorator(render_to("core_index.html"))
+    def get(self, req):
+        """todo: Docstring for get
+
+        :param req: arg description
+        :type req: type description
+        :return:
+        :rtype:
+        """
+        return super(index, self).get(req)
+    #get()
+#index
